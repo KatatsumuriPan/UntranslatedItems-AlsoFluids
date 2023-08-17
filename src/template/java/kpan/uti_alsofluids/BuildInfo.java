1aa72aead6acf7bd6a47e471c8a5bd98f2c98b21
KatatsumuriPan <katatsumuri.pan@gmail.com>
2023-08-17 10:25:51 +0900

    バージョンシステム更新

---
+++ src/template/java/kpan/uti_alsofluids/BuildInfo.java
@@ -0,0 +1,8 @@
+package kpan.uti_alsofluids;
+
+public class BuildInfo {
+	public static final String VERSION = "${version}";
+	public static final String MAJOR_VERSION = "${majorVersion}";
+	public static final String MINOR_VERSION = "${minorVersion}";
+	public static final String PATCH_VERSION = "${patchVersion}";
+}
