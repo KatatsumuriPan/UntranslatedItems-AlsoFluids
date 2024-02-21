06364193193fe7855ff89855a6980299290f94cb
KatatsumuriPan <Katatsumuri.pan@gmail.com>
2024-02-21 15:58:13 +0900

    1.12.2の開発はデフォルトブランチから1.12.2forgeへ移動しました

--- src/template/java/kpan/uti_alsofluids/BuildInfo.java
+++
@@ -1,8 +0,0 @@
-package kpan.uti_alsofluids;
-
-public class BuildInfo {
-	public static final String VERSION = "${version}";
-	public static final String MAJOR_VERSION = "${majorVersion}";
-	public static final String MINOR_VERSION = "${minorVersion}";
-	public static final String PATCH_VERSION = "${patchVersion}";
-}
